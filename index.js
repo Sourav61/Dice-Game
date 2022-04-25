4b917c1e538348913ed7c8c048d90063b75b7fa6
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-25 22:21:00 +0530

    final commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
-document.querySelector(".img1").src = "file:///D:/Desktop/coding/WebDev%20Angela/Angela-Yu-Web-Dev-Bootcamp/Dicee%20Challenge%20-%20Starting%20Files/images/dice" + randomNumber1 + ".png"
+document.querySelector(".img1").src = "https://github.com/Sourav61/Dice-Game/blob/main/images/dice" + randomNumber1 + ".png"
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber2);
-document.querySelector(".img2").src = "file:///D:/Desktop/coding/WebDev%20Angela/Angela-Yu-Web-Dev-Bootcamp/Dicee%20Challenge%20-%20Starting%20Files/images/dice" + randomNumber2 + ".png"
+document.querySelector(".img2").src = "https://github.com/Sourav61/Dice-Game/blob/main/images/dice" + randomNumber2 + ".png"
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Won!";
+  document.querySelector("h1").innerHTML = "🚩Player 1 Won!";
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Won!";
+  document.querySelector("h1").innerHTML = "🚩Player 2 Won!";
 } else {
   document.querySelector("h1").innerHTML = "Ohh! That's a draw!!";
   document.querySelector("h1").classList.add("small");
